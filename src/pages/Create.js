1782f695871b668e981e012292c50ea3326c0699
pratap360 <pratapparui0360@gmail.com>
2023-06-24 16:46:56 +0530

    first commit

---
+++ src/pages/Create.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Typography  from '@mui/material/Typography'
+
+export default function Create() {
+  return (
+    <div>
+      <Typography 
+          varient="h6"
+          color="textSecondary"
+          component="h2"
+          gutterbutton>
+          Create a new note
+      </Typography>
+    </div>
+  )
+}

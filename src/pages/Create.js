a5fe9c8f045d3120f53dc273efe7e60777149caf
pratap360 <pratapparui0360@gmail.com>
2023-06-24 20:06:39 +0530

    makestyles custom hookss

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -5,12 +5,37 @@ import Container from '@mui/material/Container';
 // import ButtonGroup from '@mui/material/ButtonGroup';
 // import SendIcon from '@mui/icons-material/Send';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { makeStyles } from '@mui/styles';
 
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      background: 'blue'
+    },
+    // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    // border: 0,
+    // borderRadius: 3,
+    // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    // color: 'white',
+    // height: 48,
+    // padding: '0 30px',
+    },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20,
+  }
+})
 
 export default function Create() {
+
+  const classes = useStyles();
+
   return (
     <Container>
       <Typography 
+          className={classes.title}
           varient="h6"
           color="textSecondary"
           component="h2"
@@ -19,6 +44,7 @@ export default function Create() {
       </Typography>
 
       <Button 
+      className={classes.btn}
       onClick={()=> console.log('u click me')}
       type='submit'
       variant='contained'

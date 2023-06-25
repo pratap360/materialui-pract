36a329c55ea071c753f1b3f7f10157edec594b60
pratap360 <pratapparui0360@gmail.com>
2023-06-25 17:31:15 +0530

    custom theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+// import { createMuiTheme, ThemeProvider } from '@mui/core'
+import { createTheme ,ThemeProvider } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+
+
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fefefe'
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  }
+})
+
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Router>
       <Switch>
         <Route exact path="/">
@@ -14,6 +38,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
+    </ThemeProvider>
   );
 }
 

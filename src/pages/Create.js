24f830c2d033545128b15fa34c629b7ac915cfcf
pratap360 <pratapparui0360@gmail.com>
2023-06-25 18:22:31 +0530

    text feilds

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,30 +1,22 @@
 import React from 'react'
-import Typography  from '@mui/material/Typography'
+import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-// import ButtonGroup from '@mui/material/ButtonGroup';
-// import SendIcon from '@mui/icons-material/Send';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { makeStyles } from '@mui/styles';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react'
+// import ButtonGroup from '@mui/material/ButtonGroup';
+// import SendIcon from '@mui/icons-material/Send';
+
+
+
 
 const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: 'violet',
-    '&:hover': {
-      background: 'blue'
-    },
-    // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    // border: 0,
-    // borderRadius: 3,
-    // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    // color: 'white',
-    // height: 48,
-    // padding: '0 30px',
-    },
-  title: {
-    textDecoration: 'underline',
-    marginBottom: 20,
+  field: {
+    marginTop: 50,
+    marginBottom: 50,
+    display: 'block'
   }
 })
 
@@ -32,29 +24,79 @@ export default function Create() {
 
   const classes = useStyles();
 
+  const [title, setTitle] = useState('')
+  const [detail, setDetail] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailError, setDetailsError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false)
+    setDetailsError(false)
+
+    if (title == '') {
+      setTitleError(true)
+    }
+
+    if (detail == '') {
+      setDetailsError(true)
+    }
+
+    if (title && detail) {
+      console.log(title, detail);
+    }
+
+  }
+
   return (
-    <Container>
-      <Typography 
-          className={classes.title}
-          varient="h6"
-          color="textSecondary"
-          component="h2"
-          gutterbutton>
-          Create a new note
+    <Container size='sm'>
+      <Typography
+        varient="h6"
+        color="textSecondary"
+        component="h2"
+        gutterbutton
+        >
+        Create a new note
       </Typography>
 
-      <Button 
-      className={classes.btn}
-      onClick={()=> console.log('u click me')}
-      type='submit'
-      variant='contained'
-      color='secondary'
-      // disableElevation
-      // startIcon={<SendIcon/>}
-      endIcon={<KeyboardArrowRightIcon/>}
-      >
-        Submit
-      </Button>
+
+      <form noValidate autoComplete='off' onSubmit={handleSubmit} >
+        <TextField
+          className={classes.field}
+          onChange={(e) => setTitle(e.target.value)}
+          label="Note Title"
+          variant='outlined'
+          color='secondary'
+          required
+          fullWidth
+          error={titleError}
+        />
+
+        <TextField
+          className={classes.field}
+          onChange={(e) => setDetail(e.target.value)}
+          label="Details"
+          variant='outlined'
+          color='secondary'
+          required
+          fullWidth
+          error={detailError}
+          multiline
+          rows={4}
+        />
+        <Button 
+          type='submit'
+          variant='contained'
+          color='secondary'
+          // disableElevation
+          // startIcon={<SendIcon/>}
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
+
+
 
       {/* <Button type='submit'>Submit</Button>
      
